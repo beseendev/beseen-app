0032063bf3faabdb0cbf69cb0f493b12a7626495
Emerson Cardoso da Trindade <emerson.cardoso.trindade@gmail.com>
2025-10-18 15:52:04 -0300

    Ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then(m => m.LoginPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage),
+    canActivate: [authGuard]
+  },
+  {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
 ];
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.beseen.com.br' // URL da sua API de produÃ§Ã£o
 };
