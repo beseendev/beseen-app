9c7335f3b7080b591f1b62acb0c4b958bf126613
Merssoo <emerson.cardoso.trindade@gmail.com>
2026-05-30 11:11:18 -0300

    #:Ajustes para produção

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,18 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
-const IS_PROD = process.argv.includes('prod');
-
 const config: CapacitorConfig = {
-  appId: IS_PROD ? 'com.beseen.app.official' : 'io.ionic.starter',
+  appId: process.env['APP_ID'] || 'io.ionic.starter',
   appName: 'BeSeen',
   webDir: 'www',
   plugins: {
     FirebaseAuthentication: {
       skipNativeAuth: false,
       providers: ['google.com'],
-      webClientId: IS_PROD 
-        ? '950779319104-1l56pob9dmnf2bkln5ap3ah961cemnrq.apps.googleusercontent.com' 
-        : '304753853961-jp8gqjgmtltheqn16nvell5i3os6k4aq.apps.googleusercontent.com'
+      webClientId: process.env['GOOGLE_WEB_CLIENT_ID'] || '304753853961-jp8gqjgmtltheqn16nvell5i3os6k4aq.apps.googleusercontent.com'
     }
   }
 };
