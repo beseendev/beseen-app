38f9a020093316e1cfd448e62bf100120d0e7309
Merssoo <emerson.cardoso.trindade@gmail.com>
2026-07-03 22:57:11 -0300

    Alteração de rotas, para o dominio correto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://beseen-api-rqzm.onrender.com/api',
+  apiUrl: 'https://api.beseen.app.br/api',
   googleClientId: '950779319104-1l56pob9dmnf2bkln5ap3ah961cemnrq.apps.googleusercontent.com',
   firebase: {
     apiKey: "AIzaSyADguV0Yp77iz0A-1Ce9IiaQID0ybiDHgA",
